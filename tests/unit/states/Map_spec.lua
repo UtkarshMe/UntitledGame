b26fec33b97319d23a35af719a32ea2a33e8c522
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-15 17:23:55 +0530

    Add states/Map tests and separate state, component tests

---
+++ tests/unit/states/Map_spec.lua
@@ -0,0 +1,6 @@
+describe('Map', function()
+    it('can be loaded from file')
+    it('can be saved to file')
+    it('allows getting and setting position of tile/character')
+    it('allows getting and setting tile/character at position')
+end)
