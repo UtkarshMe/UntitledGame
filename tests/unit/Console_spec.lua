18ba2667b5f87ddab7e326fcebcf995578e3e3fb
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-17 23:41:01 +0530

    Remove unused components

--- tests/unit/Console_spec.lua
+++ tests/unit/Console_spec.lua
@@ -1,20 +1,3 @@
 describe('Console', function()
-
-    local console = require('models.Console')
-    local width, height = 60, 80
-
-    setup(function()
-        console:init(width, height)
-    end)
-
-    it('can be instantiated', function()
-        assert.is_not_nil(console)
-    end)
-
-    it('can hold the user script', function()
-        local script = [[loop forward endloop]]
-        assert.is_nil(console.script)
-        console:setScript(script)
-        assert.is_same(script, console:getScript())
-    end)
+    it('can hold the user script')
 end)
