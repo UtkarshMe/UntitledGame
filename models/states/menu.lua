319bd1999a1107adf32245b3b5c62a2ce517255f
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-17 17:35:50 +0530

    Implement state switching and logging

--- models/states/menu.lua
+++ models/states/menu.lua
@@ -1,10 +1,12 @@
 -- models/states/menu.lua : State info for menu
 
+local log = require('log')
 local State = require('models.states.State')
 local state = State:new('menu')
 
 state:addEvent('activate', function(args)
-    print('menu activated: ' .. unpack(args))
+    log.debug('menu activated: ' .. unpack(args))
+    globals.game.switch('map')
 end)
 
 return state
