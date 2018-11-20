f9844740511ba68fc979e709b571cbb0995491fe
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-20 18:19:16 +0530

    Add tests for MenuItem model

--- tests/unit/MenuItem_spec.lua
+++ tests/unit/MenuItem_spec.lua
@@ -1,6 +1,31 @@
+local MenuItem = require('models.MenuItem')
+
 describe('Menu Item', function()
-    it('can be instantiated')
-    it('has a label')
-    it('has a target')
-    it('calls target when activated')
+    local menuItem = nil
+
+    it('can get/set a label', function()
+        local label = 'some label'
+
+        menuItem = MenuItem:new(label)
+        assert.is_same(menuItem:getLabel(), label)
+
+        label = 'some other label'
+        menuItem:setLabel(label)
+        assert.is_same(menuItem:getLabel(), label)
+    end)
+
+    it('can get/set a target', function()
+        local target = function() return true end
+
+        menuItem = MenuItem:new(nil, target)
+        local ret = menuItem:getTarget()
+        assert.is_same(ret, target)
+        assert.is_true(ret())
+
+        target = function() return nil end
+        menuItem:setTarget(target)
+        ret = menuItem:getTarget()
+        assert.is_same(ret, target)
+        assert.is_nil(ret())
+    end)
 end)
