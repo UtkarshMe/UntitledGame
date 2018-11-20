c2f59ce3c75bda87bc7d652ca0c181e907211123
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-20 14:29:15 +0530

    Add tests for Console model

--- tests/unit/Console_spec.lua
+++ tests/unit/Console_spec.lua
@@ -1,3 +1,16 @@
+local console = nil
+local script = 'some script'
+
 describe('Console', function()
-    it('can hold the user script')
+    setup(function()
+        console = require('models.Console')
+    end)
+
+    it('can get and update user script', function()
+        console:updateValue(script)
+        assert.is_same(console:getValue(), script)
+
+        console:updateValue()
+        assert.is_nil(console:getValue())
+    end)
 end)
