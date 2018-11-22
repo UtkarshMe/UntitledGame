f241da5a83629dded02646766755c14f8d2547b0
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-22 07:12:45 +0530

    Remove dead code from main

--- main.lua
+++ main.lua
@@ -7,14 +7,6 @@ local game = require('game'); globals.game = game
 -- Put config parsing, state initialization etc. here
 function love.load()
     game.load()
-
-    -- FIXME: should be in update
-    game.event.push('update', { 'forward forward forward' })
-    game.event.push('submit')
-    game.event.push('step')
-    game.event.push('step')
-    game.event.push('step')
-    game.event.push('step')
 end
 
 -- Function to render the game screen
@@ -26,10 +18,6 @@ end
 -- Function to manipulate game state at each frame
 -- Put game strategy here
 function love.update()
-    -- check for input
-    -- handle input: update state
-
-    -- update computer controlled characters
 end
 
 function love.keypressed(key, unicode)
@@ -37,5 +25,5 @@ function love.keypressed(key, unicode)
         love.event.quit()
     else
         game.event.push('keyinput', { key, unicode })
-    end  -- temporary
+    end
 end
