4fa132b332db116433cdb7337487fb1ade74d4d3
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-22 07:13:02 +0530

    Push event to love if not handled by current state

--- util.lua
+++ util.lua
@@ -7,6 +7,8 @@ function util.newEventHandler(parent, model)
     return function(event, args)
         if obj[event] then
             obj[event](model, args)
+        else
+            love.event.push(event, args)
         end
     end
 end
