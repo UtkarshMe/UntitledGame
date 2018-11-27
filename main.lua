3fb71d1db47a026a9122252dd6e809aba67bd628
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-27 16:16:21 +0530

    Allow scaling screen with changing size

--- main.lua
+++ main.lua
@@ -8,14 +8,25 @@ local game = require('game'); globals.game = game
 -- Function to initialize game state
 -- Put config parsing, state initialization etc. here
 function love.load()
+    globals.view = {
+        scale = 1,
+        x = 0,
+        y = 0,
+    }
     globals.assets.load()
     game.load()
+    globals.canvas = love.graphics.newCanvas()
 end
 
 -- Function to render the game screen
 -- Parse game state and construct the screen here
 function love.draw()
-    game.draw()
+    love.graphics.setCanvas(globals.canvas)
+        love.graphics.clear()
+        game.draw()
+    love.graphics.setCanvas()
+    love.graphics.draw(globals.canvas, globals.view.x, globals.view.y, 0,
+            globals.view.scale)
 end
 
 -- Function to manipulate game state at each frame
@@ -42,10 +53,17 @@ function love.mousepressed(x, y, button)
 end
 
 function love.resize(width, height)
-    for _,view in pairs(game.views) do
-        view:load(width, height)
-        if view.update then
-            view:update()
+    local function min(a, b)
+        if a > b then
+            return b
+        else
+            return a
         end
     end
+    local scale = min(width / 800, height / 600)
+    globals.view = {
+        scale = scale,
+        x = (width - (800 * scale)) / 2,
+        y = (height - (600 * scale)) / 2,
+    }
 end
