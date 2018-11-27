cd4edd8a9ffb631d06d32a0ea51982d4ccae42e7
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-27 13:40:40 +0530

    Allow displaying console script in map

--- views/Map.lua
+++ views/Map.lua
@@ -7,34 +7,28 @@ function view:load(width, height, props)
     self.width = math.floor(width) or 1000
     self.height = math.floor(height) or 1000
     self._props = props
-    self.canvas = love.graphics.newCanvas(self.width, self.height)
-    self.playerCanvas = love.graphics.newCanvas(self.width, self.height)
 
     self.mapArea = {
         x = 0,
         y = 0,
-        width = 4 * globals.assets.tile.width,
-        height = 4 * globals.assets.tile.height,
-        scale = { x = 1, y = 1 },
+        width = self.width,
+        height = math.floor(self.height * 0.75),
     }
 
-    love.graphics.setCanvas(self.canvas)
-        love.graphics.clear()
-        love.graphics.setBlendMode('alpha')
-    love.graphics.setCanvas()  -- reset the canvas
+    self.scriptArea = {
+        x = 0,
+        y = self.mapArea.x + self.mapArea.height,
+        width = self.width,
+        height = math.floor(self.height * 0.25),
+    }
+
 end
 
 function view:update()
     local size = self._parent:getSize()
 
-    local mapWidth =
-            globals.assets.tile.width * self.mapArea.scale.x * size.width
-    local mapHeight =
-            globals.assets.tile.height * self.mapArea.scale.y * size.height
-    self.mapArea.x = (self.width - mapWidth) / 2
-    self.mapArea.y = (self.height - mapHeight) / 2
-
-    love.graphics.setCanvas(self.canvas)
+    self.mapArea.canvas = love.graphics.newCanvas()
+    love.graphics.setCanvas(self.mapArea.canvas)
         love.graphics.setColor(1, 1, 1, 1)
         for x=1,size.width do
             for y=1,size.height do
@@ -49,19 +43,26 @@ function view:update()
 end
 
 function view:draw()
+    local user = self._parent:getPosition('user')
+    local userOnMap = {
+        x = (user[1] - 1) * globals.assets.tile.width,
+        y = (user[2] - 1) * globals.assets.tile.height
+    }
 
     love.graphics.setBackgroundColor(0, 0, 0, 1)
     love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.draw(self.canvas, self.mapArea.x, self.mapArea.y, 0,
-            self.mapArea.scale.x, self.mapArea.scale.y)
+    love.graphics.clear()
 
-    local user = self._parent:getPosition('user')
-    love.graphics.draw(globals.assets.images.user.tile,
-            self.mapArea.x
-            + (user[1] - 1) * globals.assets.tile.width * self.mapArea.scale.x,
-            self.mapArea.y
-            + (user[2] - 1) * globals.assets.tile.height * self.mapArea.scale.y,
-            0, self.mapArea.scale.x, self.mapArea.scale.y)
+    -- map area
+    love.graphics.draw(self.mapArea.canvas, 0, 0)
+
+    -- user tile
+    love.graphics.draw(globals.assets.images.user.tile, userOnMap.x,
+            userOnMap.y)
+
+    -- script console
+    love.graphics.printf(globals.game.script.raw, self.scriptArea.x,
+            self.scriptArea.y, self.scriptArea.width)
 end
 
 return view
