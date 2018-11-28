1be2a8858d56659dc4f94fa69a109be01b9e6fad
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-11-29 01:35:03 +0530

    Implement button in console

--- views/Map.lua
+++ views/Map.lua
@@ -1,6 +1,7 @@
 -- views/Map.lua : View for Map model
 
 local animate = require('animate')
+local util = require('views.util')
 local View = require('views.View')
 local view = View:new()
 
@@ -28,7 +29,7 @@ function view:load(width, height, props)
             height = 0,
         },
         user = {
-            animate = animate:new({ speed = 2, factor = 2 }),
+            animate = animate:new({ speed = 5, factor = 2 }),
         },
     }
     self.mapPeak.width = self.mapPeak.tiles.x * globals.assets.tile.width
@@ -55,14 +56,182 @@ function view:load(width, height, props)
         x = 0,
         y = self.mapPeak.y + self.mapPeak.height,
         width = self.width,
-        height = math.floor(self.height * 0.25),
-        font = love.graphics.newFont(20),
-        cursor = animate:new(),
-        padding = {
-            x = 50,
-            y = 50,
+        height = self.height - self.mapPeak.y - self.mapPeak.height,
+        editor = {
+            x = 0,
+            y = 0,
+            width = self.width - 200,
+            height = self.height - self.mapPeak.y - self.mapPeak.height,
+            cursor = animate:new(),
+            padding = {
+                x = 20,
+                y = 20,
+                color = { 0.20, 0.20, 0.20, 1 },
+            },
+            margin = {
+                x = 10,
+                y = 10,
+            },
+            border = {
+                outer = {
+                    width = 2,
+                    height = 2,
+                    color = { 0.25, 0.25, 0.25, 1 },
+                },
+                inner = {
+                    width = 3,
+                    height = 3,
+                    color = { 0.30, 0.30, 0.30, 1 },
+                },
+                width = 5,
+                height = 5,
+            },
+            input = {
+                font = love.graphics.newFont(20),
+                color = {
+                    text = { 1, 1, 1, 1 },
+                },
+            },
+        },
+        buttons = {
+            height = 50,
+            padding = {
+                x = 10,
+                y = 10,
+            },
+            data = {
+                {
+                    label = 'Run',
+                    callback = function()
+                        globals.game.event.push('submit')
+                    end
+                },
+                {
+                    label = 'Reset',
+                    callback = function()
+                        print('this is unfinished')
+                    end
+                },
+            },
+            margin = {
+                x = 10,
+                y = 10,
+            },
+            font = love.graphics.newFont(25),
+            color = {
+                background = { 0.20, 0.20, 0.20, 1},
+                font =       { 0.80, 0.80, 0.80, 1 },
+                border = {
+                    outer = { 0.25, 0.25, 0.25, 1 },
+                    inner = { 0.30, 0.30, 0.30, 1 },
+                }
+            },
+            align = 'center',
         },
     }
+
+    self.console.editor.width = self.console.editor.width
+            - 2 * self.console.editor.margin.x
+    self.console.editor.height = self.console.editor.height
+            - 2 * self.console.editor.margin.y
+
+    self.console.buttons.x = self.console.editor.x + self.console.editor.width
+            + 2 * self.console.editor.margin.x
+    self.console.buttons.y = self.console.editor.y
+    self.console.buttons.width = self.width - self.console.editor.width
+            - 2 * self.console.editor.margin.x
+
+    self.console.canvas = love.graphics.newCanvas(self.console.width,
+            self.console.height)
+    self.console.editor.canvas = love.graphics.newCanvas(
+            self.console.editor.width, self.console.editor.height)
+
+    local i = 1
+    for id,_ in pairs(self.console.buttons.data) do
+        self.console.buttons.data[id].x = self.console.buttons.x
+        self.console.buttons.data[id].y = self.console.buttons.y
+                + (i - 1) * self.console.buttons.height
+                + i * self.console.buttons.margin.y
+        self.console.buttons.data[id].width = self.console.buttons.width
+                - self.console.buttons.margin.x
+        self.console.buttons.data[id].height = self.console.buttons.height
+        i = i + 1
+    end
+
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setCanvas(self.console.editor.canvas)
+        -- border outer
+        love.graphics.setColor(self.console.editor.border.outer.color)
+        love.graphics.rectangle('fill',
+                self.console.editor.x, self.console.editor.y,
+                self.console.editor.width, self.console.editor.height)
+
+        -- border inner
+        love.graphics.setColor(self.console.editor.border.inner.color)
+        love.graphics.rectangle('fill',
+                self.console.editor.border.outer.width,
+                self.console.editor.border.outer.height,
+                self.console.editor.width
+                    - 2 * self.console.editor.border.outer.width,
+                self.console.editor.height
+                    - 2 * self.console.editor.border.outer.height)
+
+        -- padded area
+        love.graphics.setColor(self.console.editor.padding.color)
+        love.graphics.rectangle('fill', self.console.editor.border.width,
+                self.console.editor.border.height,
+                self.console.editor.width
+                    - 2 * self.console.editor.border.width,
+                self.console.editor.height
+                    - 2 * self.console.editor.border.height)
+    love.graphics.setCanvas()
+
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setCanvas(self.console.canvas)
+        -- editor
+        love.graphics.draw(self.console.editor.canvas,
+                self.console.editor.margin.x,
+                self.console.editor.margin.y)
+
+        -- buttons
+        for _,button in ipairs(self.console.buttons.data) do
+            love.graphics.setColor(self.console.buttons.color.border.outer)
+            love.graphics.rectangle('fill', button.x, button.y,
+                    button.width, button.height)
+
+            love.graphics.setColor(self.console.buttons.color.border.inner)
+            love.graphics.rectangle('fill', button.x + 2, button.y + 2,
+                    button.width - 4, button.height - 4)
+
+            love.graphics.setColor(self.console.buttons.color.background)
+            love.graphics.rectangle('fill', button.x + 5, button.y + 5,
+                    button.width - 10, button.height - 10)
+
+            love.graphics.setColor(self.console.buttons.color.font)
+            love.graphics.setFont(self.console.buttons.font)
+            love.graphics.printf(button.label,
+                    button.x + self.console.buttons.padding.x,
+                    button.y + self.console.buttons.padding.y,
+                    self.console.buttons.width
+                        - 2 * self.console.buttons.padding.x
+                        - 2 * self.console.buttons.margin.x,
+                    self.console.buttons.align)
+        end
+    love.graphics.setCanvas()
+
+    globals.game.event.add('Map', 'mousepress', function(_, args)
+        local x, y = unpack(args)
+        local offset = {
+            x = self.console.x,
+            y = self.console.y,
+        }
+        for _,button in pairs(self.console.buttons.data) do
+            if util.is_over({ x = x, y = y }, button, offset) then
+                button.callback()
+                break
+            end
+        end
+    end)
 end
 
 function view:update()
@@ -85,9 +254,9 @@ function view:update()
                 local tileName = self._parent.map.components[tile]
                 love.graphics.draw(globals.assets.images[tileName].tile,
                         (x - self.mapPeak.tiles.offset.x)
-                        * globals.assets.tile.width,
+                            * globals.assets.tile.width,
                         (y - self.mapPeak.tiles.offset.y)
-                        * globals.assets.tile.height)
+                            * globals.assets.tile.height)
             end
         end
     love.graphics.setCanvas()  -- reset the canvas
@@ -113,19 +282,26 @@ function view:draw()
             userOnMap.x, userOnMap.y)
 
     -- story
-    love.graphics.setFont(self.console.font)
     love.graphics.printf(self.story.animate:teletype(self.story.text),
             self.story.x + self.story.padding.x,
             self.story.y + self.story.padding.y,
             self.story.width, self.story.align)
 
     -- console
-    love.graphics.setFont(self.console.font)
+    love.graphics.draw(self.console.canvas, self.console.x, self.console.y)
+
+    -- editor area
+    love.graphics.setFont(self.console.editor.input.font)
+    love.graphics.setColor(self.console.editor.input.color.text)
     love.graphics.printf(self._parent.console:getValue()
-            .. self.console.cursor:blinkText('_'),
-            self.console.x + self.console.padding.x,
-            self.console.y + self.console.padding.y,
-            self.console.width)
+            .. self.console.editor.cursor:blinkText('_'),
+            self.console.x + self.console.editor.x
+                + self.console.editor.margin.x
+                + self.console.editor.padding.x,
+            self.console.y + self.console.editor.y
+                + self.console.editor.margin.y
+                + self.console.editor.padding.y,
+            self.console.editor.width)
 end
 
 return view
--- views/util.lua
+++ views/util.lua
@@ -1,8 +1,11 @@
 local util = {}
 
-function util.is_over(obj1, obj2)
-    if  obj1.x > obj2.x and obj1.x < obj2.x + obj2.width
-    and obj1.y > obj2.y and obj1.y < obj2.y + obj2.height then
+function util.is_over(obj1, obj2, offset)
+    offset = offset or { x = 0, y = 0 }
+    if  obj1.x > obj2.x + offset.x
+            and obj1.x < obj2.x + obj2.width + offset.x
+            and obj1.y > obj2.y + offset.y
+            and obj1.y < obj2.y + obj2.height + offset.y then
         return true
     else
         return false
